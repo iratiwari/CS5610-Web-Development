fe63e2c39afbec7d50318a5cffc32793523a9a7f
ira025 <ira025@ccs.neu.edu>
2015-04-08 18:05:22 -0400

    Uploaded the code and updated blog entries

---
+++ essential/essential/experiments/jQueryUIetc/Experiment3.js
@@ -0,0 +1,18 @@
+﻿function canvas() {
+    var ctx = document.getElementById('canvas').getContext('2d');
+    ctx.canvas.addEventListener('mousemove', function (event) {
+        var cursorX = event.clientX - ctx.canvas.offsetLeft;
+        var cursorY = event.clientY - ctx.canvas.offsetTop;
+        var status = document.getElementById('CurrentStatus');
+        status.innerHTML = "Current cursor Position : (" + cursorX + ", " + cursorY + ")";
+    });
+    ctx.canvas.addEventListener('click', function (event) {
+        var cursorX = event.clientX - ctx.canvas.offsetLeft;
+        var cursorY = event.clientY - ctx.canvas.offsetTop;
+        ctx.fillStyle = "yellow";
+        ctx.fillRect(cursorX - 10, cursorY - 10, 10, 10);
+    });
+}
+window.addEventListener('load', function (event) {
+    canvas();
+});
---
+++ essential/essential/experiments/jQueryUIetc/Experiment5.js
@@ -0,0 +1,11 @@
+﻿var keylist = "abcdefghijklmnopqrstuvwxyz@!#$%&*ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+var temp = ''
+function stringcreator(length) {
+    document.string.output.value = randomoutput(length)
+}
+function randomoutput(length) {
+    temp = ''
+    for (i = 0; i < length; i++)
+        temp += keylist.charAt(Math.floor(Math.random() * keylist.length))
+    return temp
+}
